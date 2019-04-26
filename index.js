672cd5199b5ee301bf8b5ee45b5e4b2fa54e91d1
Tristan Grovender <therealtristangrovender@gmail.com>
2019-04-26 12:15:08 -0500

    npm scripts implemented, CRUD operations functional

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+// Import Express
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
+const cors = require('cors');
+
+// Routes
+const projectRoutes = require('./routes/projects/projectRoutes');
+const actionRoutes = require('./routes/actions/actionRoutes');
+
+// Initialize Express
+const server = express();
+
+// Middleware
+server.use(express.json());
+// implement helmet here
+// implement morgan here 
+// implement cors here 
+
+// Use Routes
+server.use('/api/projects', projectRoutes);
+server.use('/api/actions', actionRoutes);
+
+// Test
+server.use('/', (req, res) => {
+    res.status(200).send("It's aliiivvveee!!!");
+});
+
+// Running Server
+const port = 4000;
+server.listen(port, () => {
+    console.log('\n** API up and running on port 4k **');
+});
+
+// Important Server Responses
+
+// 200 OK
+// 201 Created
+// 202 accepted
+// 400 bad request (client is not supplying right data)
+// 401 unauthorized
+// 404 not found 
+// 500 internal server error
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
